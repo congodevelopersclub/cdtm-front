9a03402d30e7086b5724548996fd96080a958adc
Chrissiku <chrissiku5@gmail.com>
2026-07-23 17:53:40 +0200

    ft : create markeplace theme

--- apps/marketplace/app/layout.tsx
+++ apps/marketplace/app/layout.tsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import { NextIntlClientProvider } from "next-intl"
 import { getLocale, getMessages } from "next-intl/server"
 
-import "@workspace/ui/globals.css"
+import "./globals.css"
 import {
   AuthProvider,
   QueryProvider,
